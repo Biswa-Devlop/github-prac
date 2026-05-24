68428479b2479dae0a8695da3b74d5b9ca1a25e4
Biswa-Devlop <biswaranjan.devlopr@gmail.com>
2026-05-24 17:37:21 +0530

    initial commit

---
+++ src/main/java/com/tcs/uco/UserReg.java
@@ -0,0 +1,13 @@
+package com.tcs.uco;
+
+import in.biswa.security.PwdSecurityService;
+
+public class UserReg {
+	public static void main(String[] args) {
+		PwdSecurityService p = new PwdSecurityService();
+		String encode = p.encode("Ranjan");
+		System.out.println(encode);
+
+	}
+
+}
